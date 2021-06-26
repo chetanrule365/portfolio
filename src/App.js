e6ae42f29107cce8cf022960686c6e5e8193c63e
chetan <chetanrule365@gmail.com>
2021-06-26 11:15:59 +0530

    new project added

--- src/App.js
+++ src/App.js
@@ -9,170 +9,150 @@ import Menu from "./components/Menu";
 
 function App() {
     return (
-        <div className='App'>
+        <div className="App">
             <Menu />
-            <div className='side_link'>
+            <div className="side_link">
                 <Bounce>
                     <p>
-                        <a href='mailto:chetanrule365@gmail.com'>
-                            chetanrule365@gmail.com
-                        </a>
+                        <a href="mailto:chetanrule365@gmail.com">chetanrule365@gmail.com</a>
                     </p>
                 </Bounce>
-                <div className='side_line'></div>
+                <div className="side_line"></div>
             </div>
-            <section className='my'>
+            <section className="my">
                 <Fade delay={100}>
                     <p style={{ fontWeight: 700 }}>Hi! my name is</p>
                 </Fade>
                 <Fade top cascade delay={300}>
-                    <h1 className='my_name'>N Chetan Chandra Sai Reddy</h1>
+                    <h1 className="my_name">N Chetan Chandra Sai Reddy</h1>
                 </Fade>
                 <Fade delay={500}>
-                    <h2 className='my_prof'>
-                        I build Web and Mobile Applications
-                    </h2>
+                    <h2 className="my_prof">I build Web and Mobile Applications</h2>
                 </Fade>
                 <Fade delay={700}>
-                    <p className='my_desc'>
+                    <p className="my_desc">
                         I am a web and mobile app developer, specialized in
                         <br /> React Js and Ionic frameworks.
-                        <br />I am currently working as a{" "}
-                        <span>Front-end developer</span> at{" "}
-                        <a
-                            href='http://www.truexam.in'
-                            target='_blank'
-                            rel='noopener noreferrer'>
+                        <br />I am currently working as a <span>Front-end developer</span> at{" "}
+                        <a href="http://www.truexam.in" target="_blank" rel="noopener noreferrer">
                             <span>Truexam.</span>
                         </a>
                     </p>
                 </Fade>
                 <Fade delay={1000}>
-                    <a href='mailto:chetanrule365@gmail.com' className='button'>
+                    <a href="mailto:chetanrule365@gmail.com" className="button">
                         Get in touch
                     </a>
                 </Fade>
             </section>
             <AboutMe />
-            <section id='experience'>
-                <div className='title'>
+            <section id="experience">
+                <div className="title">
                     <p>Where I've worked</p>
-                    <div className='line'></div>
+                    <div className="line"></div>
                 </div>
                 <p style={{ margin: "30px 0" }}>
-                    I am currently working as a <span>Front-end developer</span>{" "}
-                    at{" "}
-                    <a
-                        href='http://www.truexam.in'
-                        target='_blank'
-                        rel='noopener noreferrer'>
+                    I am currently working as a <span>Front-end developer</span> at{" "}
+                    <a href="http://www.truexam.in" target="_blank" rel="noopener noreferrer">
                         <span>Truexam</span>
                     </a>
                 </p>
             </section>
-            <section id='projects'>
-                <div className='title'>
+            <section id="projects">
+                <div className="title">
                     <p>Some things I've built</p>
-                    <div className='line'></div>
+                    <div className="line"></div>
                 </div>
+                <ProjectCard2
+                    title="Speak And Share App"
+                    desc="An ionic app to Translate and Share the spoken"
+                    img_path={require("./assets/speak-and-share.jpg")}
+                    tech_used={["Ionic", "React js", "Google API"]}
+                    link="https://play.google.com/store/apps/details?id=com.shadowmind.speakandshare"
+                />
                 <ProjectCard
-                    title='Front end work blog site.'
-                    desc='A hybrid app to learn skills and to publish blogs.'
+                    title="Front end work blog site."
+                    desc="A Web App to learn and publish blogs."
                     img_path={require("./assets/few_blogsite.jpeg")}
                     tech_used={["Ionic", "React Js", "Firebase"]}
-                    link='https://frontendwork.netlify.app/'
+                    link="https://frontendwork.netlify.app/"
                 />
                 <ProjectCard2
-                    title='Tindarr-game.'
-                    desc='An ionic app where it has swipable cards to play.'
+                    title="Tindarr-game."
+                    desc="An ionic app where it has swipable cards to play."
                     img_path={require("./assets/tindarr-game.jpeg")}
                     tech_used={["Ionic", "React js"]}
-                    link='https://tindarr-game.netlify.app/'
+                    link="https://tindarr-game.netlify.app/"
                 />
                 <ProjectCard
-                    title='Image-editing-bootcamp Assignment'
-                    desc='A web app that helps instructors in the Image-editing-bootcamp for creating tasks and review submissions which are submitted by the students.'
+                    title="Image-editing-bootcamp Assignment"
+                    desc="A web app that helps instructors in the Image-editing-bootcamp for creating tasks and review submissions which are submitted by the students."
                     img_path={require("./assets/bootcamp.jpeg")}
                     tech_used={["React Js", "SCSS"]}
-                    link='https://chetanrule365.github.io/Image-editing-bootcamp'
+                    link="https://chetanrule365.github.io/Image-editing-bootcamp"
                 />
                 <ProjectCard2
-                    title='Android 11 Beta Replicate'
-                    desc='I dupliacted the Android 11 Beta page to text my CSS skills.'
+                    title="Android 11 Beta Replicate"
+                    desc="I dupliacted the Android 11 Beta page to text my CSS skills."
                     img_path={require("./assets/android_replicate.png")}
                     tech_used={["HTML", "CSS", "JavaScript"]}
-                    link='https://chetanrule365.github.io/android11_web_page_replicate'
+                    link="https://chetanrule365.github.io/android11_web_page_replicate"
                 />
                 <ProjectCard
-                    title='Today ToDo App'
-                    desc='A native app for managing all your todos, events, and get scheduled notifications.'
+                    title="Today ToDo App"
+                    desc="A Flutter app for managing all your todos, events, and get scheduled notifications."
                     img_path={require("./assets/today_todo_app.jpg")}
                     tech_used={["Flutter", "Firebase"]}
-                    link='https://play.google.com/store/apps/details?id=com.MyToDos.in'
+                    link="https://play.google.com/store/apps/details?id=com.MyToDos.in"
                 />
                 <ProjectCard2
-                    title='Music Streamer'
+                    title="Music Streamer"
                     desc="It's a music streaming web app to listen music online."
                     img_path={require("./assets/music_streamer.png")}
                     tech_used={["React Js", "Howler Js", "CSS"]}
-                    link='https://music-streamer.netlify.app'
+                    link="https://music-streamer.netlify.app"
                 />
                 <ProjectCard
-                    title='Alumni'
-                    desc='A web app for alumni of mechanical department students in JNTUACEP.'
+                    title="Alumni"
+                    desc="A website for alumni of mechanical department students in JNTUACEP."
                     img_path={require("./assets/alumni.jpeg")}
                     tech_used={["HTML", "CSS", "JavaScript", "PHP"]}
-                    link='https://alumnimechjntuacep.in'
+                    link="https://alumnimechjntuacep.in"
                 />
             </section>
-            <section id='contact'>
-                <p className='end_title'>Get in Touch</p>
+            <section id="contact">
+                <p className="end_title">Get in Touch</p>
                 <p>
-                    I'm currently looking for new oppurtunities, my inbox is
-                    always open. Whether you have a question or just want to say
-                    hi, say hello!
+                    I'm currently looking for new oppurtunities, my inbox is always open. Whether you have a question or
+                    just want to say hi, say hello!
                 </p>
-                <a
-                    href='mailto:chetanrule365@gmail.com'
-                    style={{ marginTop: "100px" }}
-                    className='button'>
+                <a href="mailto:chetanrule365@gmail.com" style={{ marginTop: "100px" }} className="button">
                     Say Hello
                 </a>
             </section>
-            <section className='social'>
-                <div className='social_links'>
+            <section className="social">
+                <div className="social_links">
                     <Bounce>
-                        <a
-                            target='_blank'
-                            rel='noopener noreferrer'
-                            href='https://www.instagram.com/chetansaireddy/'>
-                            <img
-                                src={require("./assets/instagram.png")}
-                                alt=''
-                            />
+                        <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/chetansaireddy/">
+                            <img src={require("./assets/instagram.png")} alt="" />
                         </a>
                     </Bounce>
                     <Bounce delay={300}>
-                        <a
-                            target='_blank'
-                            rel='noopener noreferrer'
-                            href='https://github.com/chetanrule365'>
-                            <img src={require("./assets/github.jpg")} alt='' />
+                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/chetanrule365">
+                            <img src={require("./assets/github.jpg")} alt="" />
                         </a>
                     </Bounce>
                     <Bounce delay={700}>
                         <a
-                            target='_blank'
-                            rel='noopener noreferrer'
-                            href='https://www.linkedin.com/in/chetan-n-55338b180/'>
-                            <img
-                                src={require("./assets/linkedin.png")}
-                                alt=''
-                            />
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://www.linkedin.com/in/chetan-n-55338b180/"
+                        >
+                            <img src={require("./assets/linkedin.png")} alt="" />
                         </a>
                     </Bounce>
                 </div>
-                <div className='side_line'></div>
+                <div className="side_line"></div>
             </section>
         </div>
     );
--- src/components/ProjectCard2.js
+++ src/components/ProjectCard2.js
@@ -3,36 +3,36 @@ import { Zoom } from "react-reveal";
 function ProjectCard2({ title, desc, img_path, tech_used, link }) {
     return (
         <Zoom>
-            <div className='project_card2'>
-                <div className='content'>
-                    <p className='card_title'>{title}</p>
-                    <div className='desc'>
+            <div className="project_card2">
+                <div className="content">
+                    <p className="card_title">{title}</p>
+                    <div className="desc">
                         <p>{desc}</p>
                     </div>
-                    <div className='tech_used'>
+                    <div className="tech_used">
                         {tech_used.map((lang, i) => {
                             return <p key={i}>{lang}</p>;
                         })}
                     </div>
                     <a
                         href={link}
-                        target='_blank'
-                        rel='noopener noreferrer'
+                        target="_blank"
+                        rel="noopener noreferrer"
                         style={{
                             marginTop: "30px",
                             color: "white",
                             fontSize: "20px",
                             textDecoration: "none",
-                        }}>
-                        <i className='fa fa-external-link'></i>&nbsp;visit
-                        </a>
+                        }}
+                    >
+                        <i className="fa fa-external-link"></i>&nbsp;visit
+                    </a>
                 </div>
-                <div className='img_wrap'>
-                    <img src={img_path} alt='' />
-                    <div className='img_lay'></div>
+                <div className="img_wrap">
+                    <img src={img_path} alt="" />
+                    <div className="img_lay"></div>
                 </div>
             </div>
-
         </Zoom>
     );
 }
